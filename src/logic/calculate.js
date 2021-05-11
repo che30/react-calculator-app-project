c39f3704d806b92b6bd7f76b1d4c0cfa8f98a01e
che <che.blancoo58@gmail.com>
2021-05-11 16:47:35 +0100

    Add a case in switch method in calculate.js

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -8,6 +8,8 @@ const Calculate = ({ total, next, operator }, buttonName) => {
       return operate(total, next, buttonName);
     case 'AC':
       return { total: 0, next: 0, operator };
+    case '=' :
+    return {total: 0,next: 0,operator}
     default:
       return operate(total, next, buttonName);
   }
