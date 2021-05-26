c5bfd9ebe5d567612f6eed4018c9d6c9551e8195
che <che.blancoo58@gmail.com>
2021-05-26 23:51:53 +0100

    Readjust snapshot

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,6 +1,6 @@
 import '../Home.css';
 
 const Home = () => (
-  <h2 className="home">Welcomes to Math magican website</h2>
+  <h2 className="home">Welcome to Math magican website</h2>
 );
 export default Home;
