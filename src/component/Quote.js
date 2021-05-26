b5a677ec03b219a00ebe63a728706bc9b68008dd
che <che.blancoo58@gmail.com>
2021-05-26 23:34:45 +0100

    Update snapshot

--- src/component/Quote.js
+++ src/component/Quote.js
@@ -2,7 +2,7 @@ import '../Quote.css';
 
 const Quote = () => (
   <div className="quote">
-    <h2><q>Pure mathematics is,, in its way, the poetry of logical ideas.</q></h2>
+    <h2><q>Pure mathematics is, in its way, the poetry of logical ideas.</q></h2>
     <div>Albert Einstein</div>
   </div>
 );
