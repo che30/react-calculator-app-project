1b8a879e6ee83215b00da65dff10a334b5ba756b
che <che.blancoo58@gmail.com>
2021-05-27 11:48:05 +0100

    Add test to test button content

--- src/component/App.test.js
+++ src/component/App.test.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {render, screen} from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+
 import App from './App';
 
 it('renders correctly', () => {
@@ -8,3 +12,10 @@ it('renders correctly', () => {
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
+test('type', () => {
+  render(<App />)
+  const buttonEl = screen.getByText(/1/i);
+  expect(buttonEl).toBeInTheDocument();
+  // userEvent.type(screen.getByRole('textbox'), 'Hello,{enter}World!')
+  // expect(screen.getByRole('textbox')).toHaveValue('Hello,\nWorld!')
+})
